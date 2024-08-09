00550f476e48c69ce1a75255206c3c0d42a11241
ArandaLidia <arandagomezlidia@gmail.com>
2024-08-09 02:10:10 +0200

    up

--- T4/Usuarios/utils/gestor.js
+++ T4/Usuarios/utils/gestor.js
@@ -4,11 +4,14 @@ let inputApellido = document.querySelector("input#input-apellido")
 let boton = document.querySelector("#boton-agregar")
 let listaUsuarios = document.querySelector("#lista-usuarios")
 
-
+let listaUsuariosDatos = []
+let botonBuscar = document.querySelector("#boton-buscar")
+let listaBuscar = document.querySelector("#lista-busqueda")
+let inputBuscar = document.querySelector("input#input-buscar")
 boton.addEventListener("click", () => {
     if (inputNombre.value.length > 0 && inputApellido.value.length > 0) {
         //listaUsuarios.innerHTML += `<li class='list-group-item mt-2 animate__animated animate__zoomInUp'>${inputNombre.value} ${inputApellido.value}</li>`
-
+        listaUsuariosDatos.push(`${inputNombre.value} ${inputApellido.value}`)
         let nodo = document.createElement("li")
         nodo.textContent = `${inputNombre.value} ${inputApellido.value}`
         nodo.className = "list-group-item mt-2 animate__animated animate__zoomInUp"
@@ -40,4 +43,14 @@ boton.addEventListener("click", () => {
         })
     }
 
+})
+
+botonBuscar.addEventListener("click", () => {
+    listaBuscar.innerHTML = ""
+    let listaBusqueda = listaUsuariosDatos.filter((item) => {
+        return item.toLowerCase().includes(inputBuscar.value.toLowerCase());
+    })
+    listaBusqueda.forEach((item) => {
+        listaBuscar.innerHTML += `<li class='list-group-item mt-2 animate__animated animate__zoomInUp'>${item}</li>`
+    })
 })
