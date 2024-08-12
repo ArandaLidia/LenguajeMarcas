b9e21a92b5419ba5859c5d16b35836a4f9ae7183
ArandaLidia <arandagomezlidia@gmail.com>
2024-08-13 01:23:45 +0200

    up

--- T4/Repaso/utils/gestion.js
+++ T4/Repaso/utils/gestion.js
@@ -14,19 +14,17 @@ let listaSaludo = document.querySelector("#lista-saludo")
 let selectopciones = document.querySelector("#select-opciones")
 //Con addEventListener  ¡¡¡¡MEJOR ASÍ!!! 
 boton.addEventListener("click", () => {
-
-    console.log("Botón pulsado")
+    console.log("Botón pulsado");
     if (inputNombre.value.length > 0) {
         realizarSaludo(inputNombre.value, selectopciones.value);
-        guardarSaludo(inputNombre.value)
+        guardarSaludo(inputNombre.value, selectopciones.value); // Pasa nombre y opción
 
     } else {
-        alert("No ha introducido el nombre")
+        alert("No ha introducido el nombre");
     }
 
-    inputNombre.value = ""
-
-})
+    inputNombre.value = "";
+});
 
 
 //Con atributo onclick="nombrefuncion()"
@@ -43,14 +41,13 @@ function realizarSaludo(nombre, opcion) {
 
 }
 
+function guardarSaludo(nombre, opcion) {
+    // SE USA CUANDO NO HAY ANIMACIONES
+    listaSaludo.innerHTML += `<li>${nombre} (${opcion})</li>`;
 
-function guardarSaludo(nombre) {
-    //SE USA CUANDO NO HAY ANIMACIONES
-    listaSaludo.innerHTML += `<li>${inputNombre.value} ${selectopciones}</li>`
-
-    //ESTE SE USA PARA QUE LAS ANIMACIONES NO SE DUPLIQUEN. 
-    /* let nodo = document.createElement("li")
-    nodo.textContent = nombre //añado texto al nodo lista.
-    nodo.className="nombre-clase" o nodo.classList.add("nombre-clase") //añado la clase al nodo li
-    listaSaludo.append(nodo) //añado el nodo a la lista. */
+    // ESTE SE USA PARA QUE LAS ANIMACIONES NO SE DUPLIQUEN. 
+    /* let nodo = document.createElement("li");
+    nodo.textContent = `${nombre} (${opcion})`; //añado texto al nodo lista.
+    nodo.className = "nombre-clase"; //añado la clase al nodo li
+    listaSaludo.append(nodo); //añado el nodo a la lista. */
 }
